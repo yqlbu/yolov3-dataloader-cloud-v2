36af1e1373cebfa7a2d7564122db89a178b63f08
yqlbu <31861128+yqlbu@users.noreply.github.com>
2020-04-24 20:54:50 +0800

    Delete demo.sh

--- demo.sh
+++
@@ -1,9 +0,0 @@
-export DARKNET=/content/darknet
-export PRJ_PATH=/content/yolov3-dataloader-cloud
-
-cd $DARKNET
-./darknet detector demo \
-$PRJ_PATH/<NEW_MODEL>.data \
-$PRJ_PATH/<NEW_MODEL>.cfg \
-$PRJ_PATH/<YOUR_FINAL_WEIGHT_FROM_COLAB> \
--c 0 -thresh .5
